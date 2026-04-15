a79353124cfd6b1bc90e60ead52c943c7eeb44ea
hassine <triguihassine573@gmail.com>
2026-04-15 20:27:03 +0100

    Initial Django + DRF backend

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'core', 
 ]
+
+REST_FRAMEWORK = { 
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view 
+
+@api_view(['GET'])
+def health_check(request):
+    return Response({"status": "ok"}) 
+
+# Create your views here.
